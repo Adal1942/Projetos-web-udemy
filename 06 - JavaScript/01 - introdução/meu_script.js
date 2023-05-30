25b29701d0a3713bdc97c540275934ce2bf52921
Adal1942 <adautosombra1942@gmail.com>
2023-05-30 20:54:46 -0300

    introdução ao javascript

---
+++ 06 - JavaScript/01 - introdução/meu_script.js
@@ -0,0 +1,6 @@
+alert('Olá, seja bem vindo ao curso!');
+document.getElementById('nome').value = 'Oi'; //Seleciona um ele no DOM e atualiza o valor desse elemento com uma string
+
+//Comentários de uma linha
+
+/* Comentários de múltiplas linhas */
